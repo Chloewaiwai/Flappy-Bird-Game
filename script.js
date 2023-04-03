08f186277772b660ab994bef66751f6f7631b5d0
Chloewaiwai <waiyip814@gmail.com>
2023-04-03 18:35:17 +0800

    add height

--- script.js
+++ script.js
@@ -102,25 +102,30 @@ function play(){
     let pipe_gap = 35;
 
     function create_pipe(){
+        let background_container = document.querySelector('.background')
+   
+
         if(game_state != 'Play') return;
 
         if(pipe_seperation > 115){
             pipe_seperation = 0;
-
-            let pipe_posi = Math.floor(Math.random() * 43) + 8;
+            let pipe_posi = Math.floor(Math.random() * 31) + 20;
             let pipe_sprite_inv = document.createElement('div');
             pipe_sprite_inv.className = 'pipe_sprite';
-            pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
+            pipe_sprite_inv.style.height = pipe_posi+ 'vh';
             pipe_sprite_inv.style.left = '100vw';
 
-            document.body.appendChild(pipe_sprite_inv);
+            background_container.appendChild(pipe_sprite_inv);
+            let pipe_bot =Math.floor(Math.random() * 26);
             let pipe_sprite = document.createElement('div');
             pipe_sprite.className = 'pipe_sprite';
-            pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
+            pipe_sprite.style.height =  80 - pipe_gap+ 'vh';
+            pipe_sprite.style.backgroundPosition =  'top';
+            pipe_sprite.style.top = pipe_gap + pipe_posi +'vh';
             pipe_sprite.style.left = '100vw';
             pipe_sprite.increase_score = '1';
 
-            document.body.appendChild(pipe_sprite);
+             background_container.appendChild(pipe_sprite);
         }
         pipe_seperation++;
         requestAnimationFrame(create_pipe);
